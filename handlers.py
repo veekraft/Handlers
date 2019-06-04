ce8afb5e7b41656413c3bb21b3ba7e68516fbdea
veekraft <50915154+veekraft@users.noreply.github.com>
2019-06-04 22:14:39 +1000

    Update handlers.py
    Added to run: threaded=True

--- handlers.py
+++ handlers.py
@@ -113,4 +113,4 @@ def delete():
         return jsonify(response)
         
 if __name__ == "__main__":
-        app.run(debug=False, host='127.0.0.1', port=int(os.getenv('PORT', '5000')))
+        app.run(debug=False, host='127.0.0.1', port=int(os.getenv('PORT', '5000')), threaded=True)
